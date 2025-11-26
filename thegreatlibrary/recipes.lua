38478ea8b6381aa0865b7c6d3483e4194643d092
Edwin Fox <66983687+edwin-fox@users.noreply.github.com>
2025-11-26 14:05:44 -0300

    Delete thegreatlibrary directory

--- thegreatlibrary/recipes.lua
+++
@@ -1,9 +0,0 @@
--- Book crafting recipes
-minetest.register_craft({
-    output = "thegreatlibrary:manifest",
-    recipe = {
-        {"blueprint_worldgen:workplace_marker", "blueprint_worldgen:workplace_marker", "blueprint_worldgen:workplace_marker"},
-        {"blueprint_worldgen:workplace_marker", "default:book", "blueprint_worldgen:workplace_marker"},
-        {"blueprint_worldgen:workplace_marker", "blueprint_worldgen:workplace_marker", "blueprint_worldgen:workplace_marker"}
-    }
-})
